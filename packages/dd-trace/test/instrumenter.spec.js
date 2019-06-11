617e8728404019c051770e263a37126068665b77
rochdev <roch.devost@datadoghq.com>
2019-06-11 13:50:47 -0400

    fix lint and tests

--- packages/dd-trace/test/instrumenter.spec.js
+++ packages/dd-trace/test/instrumenter.spec.js
@@ -56,9 +56,9 @@ describe('Instrumenter', () => {
         'express-mock': integrations.express,
         'mysql-mock': integrations.mysql
       },
-      '../packages/datadog-plugin-http/src': integrations.http,
-      '../packages/datadog-plugin-express-mock/src': integrations.express,
-      '../packages/datadog-plugin-mysql-mock/src': integrations.mysql
+      '../../datadog-plugin-http/src': integrations.http,
+      '../../datadog-plugin-express-mock/src': integrations.express,
+      '../../datadog-plugin-mysql-mock/src': integrations.mysql
     })
 
     instrumenter = new Instrumenter(tracer)
